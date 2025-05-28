4788043794e1c08de9b7551514fe6fe692f36a62
chang0506 <j8970@naver.com>
2025-05-28 17:25:37 +0900

    feature/entity4

---
+++ src/main/java/com/example/seniya_back/common/enums/Specialty.java
@@ -0,0 +1,5 @@
+package com.example.seniya_back.common.enums;
+
+public enum Specialty {
+    SLEEP, REHABILITATION, EXERCISE, PSYCHOLOGY
+}
---
+++ src/main/java/com/example/seniya_back/entity/TrainerApplication.java
@@ -0,0 +1,35 @@
+package com.example.seniya_back.entity;
+
+import com.example.seniya_back.common.enums.ApprovalStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "trainer_applications")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class TrainerApplication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long applicationId;
+
+    @Column(nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @joinColumn(nullable = false)
+    private ApprovalStatus ApprovalStatus;
+
+    private LocalDate appliedDate;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private LocalDateTime updatedAt = LocalDateTime.now();
+}
