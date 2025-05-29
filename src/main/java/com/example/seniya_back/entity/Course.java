a3ccfdbb9b8a927d7cfce66acbf7ce1d199d2287
Wjkdj <wlsdnxo0807@gmail.com>
2025-05-29 16:45:48 +0900

    feature/update-entity

---
+++ src/main/java/com/example/seniya_back/entity/Course.java
@@ -0,0 +1,58 @@
+package com.example.seniya_back.entity;
+
+import com.example.seniya_back.common.enums.Category;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Table(name = "classes")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_id")
+    private Long courseId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "categry", nullable = false)
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "tariner_id")
+    private TrainerProfile trainerProfile;
+
+    @Column(nullable = false)
+    private String title;
+
+    @OneToOne(mappedBy = "Course", cascade = CascadeType.ALL)
+    private Participations participations;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(name = "class_date", nullable = false)
+    private LocalDateTime date;
+
+    @Column(name = "class_start_time", nullable = false)
+    private LocalTime startTime;
+
+    @Column(name = "class_end_time", nullable = false)
+    private LocalTime endTime;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "class_room", nullable = false)
+    private String room;
+}
