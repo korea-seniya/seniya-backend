576dae263ed5a71d0754a964738aa842f143c984
Wjkdj <wlsdnxo0807@gmail.com>
2025-06-08 22:21:52 +0900

    feat:PostService-update

--- src/main/java/com/example/seniya_back/repository/PostRepository.java
+++ src/main/java/com/example/seniya_back/repository/PostRepository.java
@@ -4,6 +4,11 @@ import com.example.seniya_back.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByTitleIgnoreCaseContaining(String title);
+
+    List<Post> findByRoleIgnoreCaseContaining(String roleName);
 }
