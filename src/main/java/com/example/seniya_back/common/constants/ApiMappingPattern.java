20a2c6d7f82bf99132ec4e541adc48f9637687d7
chang0506 <j8970@naver.com>
2025-06-11 15:50:47 +0900

    feat:ApiMappingPattern-update

--- src/main/java/com/example/seniya_back/common/constants/ApiMappingPattern.java
+++ src/main/java/com/example/seniya_back/common/constants/ApiMappingPattern.java
@@ -1,8 +1,14 @@
 package com.example.seniya_back.common.constants;
 
 public interface ApiMappingPattern {
-    // 예시
     String AUTH_API = "/api/v1/auth";
-    String INQUIRY_API = "/api/v1/inquiries";
+    String USER_API = "/api/v1/users";
+    String PAYMENT_API = "/api/v1/payments";
     String TRAINER_APPLY_API = "api/v1/trainer-applications";
+    String INQUIRY_API = "/api/v1/inquiries";
+    String POST_API = "api/v1/posts";
+    String NOTICE_API = "api/v1/notices";
+    String ADMIN_API = "api/v1/admin";
+    String PARTICIPATION_API = "api/v1/participations";
+    String COURSE_API = "api/v1/courses";
 }
