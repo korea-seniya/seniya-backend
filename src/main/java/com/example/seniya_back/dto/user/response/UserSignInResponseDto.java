1720551874ce5df4f1c170e6b2366ee7aa21f1a8
iasos2 <ykkminji@gmail.com>
2025-06-11 17:32:01 +0900

    feat:update-auth

--- src/main/java/com/example/seniya_back/dto/user/response/UserSignInResponseDto.java
+++ src/main/java/com/example/seniya_back/dto/user/response/UserSignInResponseDto.java
@@ -8,5 +8,8 @@ import lombok.Getter;
 public class UserSignInResponseDto {
 //    private String token; // jwt 토큰
      User user;
+
+     public UserSignInResponseDto(String token, User user, int exprTime) {
+     }
 //    private int exprTime; // expire + time: (토큰) 만료 시간
 }
