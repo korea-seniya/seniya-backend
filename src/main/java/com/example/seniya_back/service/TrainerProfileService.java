6efa842107430fd2d4b98909f9409d873cea5c70
chang0506 <j8970@naver.com>
2025-06-24 11:39:59 +0900

    feat:update-trainer

--- src/main/java/com/example/seniya_back/service/TrainerProfileService.java
+++ src/main/java/com/example/seniya_back/service/TrainerProfileService.java
@@ -2,16 +2,19 @@ package com.example.seniya_back.service;
 
 import com.example.seniya_back.dto.ResponseDto;
 import com.example.seniya_back.dto.trainer.requestDto.TrainerProfileRequestDto;
+import com.example.seniya_back.dto.trainer.requestDto.UpdateTrainerProfileRequestDto;
 import com.example.seniya_back.dto.trainer.responseDto.TrainerProfileCreateResponseDto;
 import com.example.seniya_back.dto.trainer.responseDto.TrainerProfileResponseDto;
 import jakarta.validation.Valid;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.naming.NoPermissionException;
+import java.io.IOException;
 
 public interface TrainerProfileService {
-    ResponseDto<TrainerProfileCreateResponseDto> createProfile(String username, @Valid TrainerProfileRequestDto dto) throws NoPermissionException;
+    ResponseDto<TrainerProfileCreateResponseDto> createProfile(String username, @Valid TrainerProfileRequestDto dto, MultipartFile file) throws NoPermissionException, IOException;
 
     ResponseDto<TrainerProfileResponseDto> getTrainerProfile(String username) throws NoPermissionException;
 
-    ResponseDto<TrainerProfileResponseDto> updateProfile(String username, @Valid TrainerProfileRequestDto dto) throws NoPermissionException;
+    ResponseDto<TrainerProfileResponseDto> updateProfile(String username, @Valid UpdateTrainerProfileRequestDto dto, MultipartFile file) throws NoPermissionException, IOException;
 }
