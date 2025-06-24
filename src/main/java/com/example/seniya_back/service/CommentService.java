0286a68e258ed754e94f6c490675866b125211ca
iasos2 <ykkminji@gmail.com>
2025-06-25 01:41:57 +0900

    feature/update-commentdto

--- src/main/java/com/example/seniya_back/service/CommentService.java
+++ src/main/java/com/example/seniya_back/service/CommentService.java
@@ -1,9 +1,13 @@
 package com.example.seniya_back.service;
 
+import com.example.seniya_back.dto.ResponseDto;
 import com.example.seniya_back.dto.comment.request.CommentCreateRequestDto;
 import com.example.seniya_back.dto.comment.request.CommentUpdateRequestDto;
 import com.example.seniya_back.dto.comment.response.CommentCreateResponseDto;
 import com.example.seniya_back.dto.comment.response.CommentUpdateResponseDto;
+import com.example.seniya_back.dto.post.response.CommentResponseDto;
+
+import java.util.List;
 
 public interface CommentService {
     CommentCreateResponseDto createComment(Long postId, CommentCreateRequestDto dto, String username);
@@ -11,4 +15,6 @@ public interface CommentService {
     CommentUpdateResponseDto updateComment(Long postId, Long commentId, CommentUpdateRequestDto dto);
 
     void deleteComment(Long postId, Long commentId);
+
+    ResponseDto<List<CommentResponseDto>> getComment(Long postId);
 }
